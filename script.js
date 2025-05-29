df256465faf4a066b0a239f27154a0168207b594
smyles4 <smyles4@cps.edu>
2025-05-29 13:52:55 +0000

    1

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 
-var input=documentquerySelector(".input")
+let input = document.querySelector(".input");
+let clearButton = document.querySelector(".clear");
+let equalsButton = document.querySelector(".equals");
+let result = document.querySelector(".result");
+
+
+const calculateDoggieAge= (doggieAge) => {
+    const humanAge= doggieAge *7;
+    console.log(humanAge)
+    result.innerHTML='your doggie is ${humanAge} years old in human years!'
+}
+
+
+equalsButton.addEventListener("click",() => {
+
+let userInput= Number(input.value)
+calculateDoggieAge(userInput)
+
+})
 
 
 
@@ -10,11 +28,8 @@ var input=documentquerySelector(".input")
 
 
 
+clearButton.onclick = function(){
+input.value="";
+result.innerHTML = "";
 
-
-
-
-
-
-
-console.log("ok");console.log("ok");
+}
